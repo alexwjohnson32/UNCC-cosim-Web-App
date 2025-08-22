8fadbfa03ab759169b59ce9736b86e80a92d4a1c
Alex Johnson <alex.johnson@corvidtec.com>
2025-08-22 15:24:32 -0500

    adding tailwind

--- client/src/components/pages/components/ComponentsPage.jsx
+++ client/src/components/pages/components/ComponentsPage.jsx
@@ -1,7 +1,6 @@
 import { Plug2, Plus, Trash2 } from "lucide-react";
 import { useState } from "react";
 import Page from "../Page";
-import "./Components.css";
 
 export default function ComponentsPage() {
     const [simName, setSimName] = useState("");
@@ -66,36 +65,47 @@ export default function ComponentsPage() {
 
     function createTransmissionNodeCard(node) {
         return (
-            <div className='card' key={node.name}>
-                <span className="card-title">GridPack - {node.name}</span>
-                <span className="card-data"><strong>ID:</strong> {node.name}</span>
-                <span className="card-data"><strong>Child Nodes:</strong> {Object.keys(node.children).length}</span>
-                <button className="rotating-button" onClick={() => addDistributionNode(node.name)}>
-                    <div className="icon-container">
-                        <Plug2 className="icon icon-plug" />
-                        <Plus className="icon icon-plus" />
+            <div className='relative flex flex-col gap-1 h-25 w-full p-4 bg-black/5 rounded-lg' key={node.name}>
+                <span className="font-bold">GridPack - {node.name}</span>
+                <span className="text-sm"><strong>ID:</strong> {node.name}</span>
+                <span className="text-sm"><strong>Child Nodes:</strong> {Object.keys(node.children).length}</span>
+                <div className="absolute right-4 bottom-4 flex flex-row gap-2 justify-center items-center">
+                    <div className="flex items-center justify-center h-6 w-6 cursor-pointer" onClick={() => removeTransmissionNode(node.name)}>
+                        <Trash2 className="h-5 w-5 text-[#fb4b4b]/75 fill-[#fb4b4b]/75 hover:text-[#fb4b4b] hover:fill-[#fb4b4b]" />
                     </div>
-                </button>
-                <div className="icon-container delete" onClick={() => removeTransmissionNode(node.name)}>
-                    <Trash2 fill="red" className="icon delete-icon" />
+                    <button
+                        onClick={() => addDistributionNode(node.name)}
+                        className="bg-corvid-primary/50 hover:bg-corvid-primary h-8 w-8 rounded-full border-0
+                    flex items-center justify-center overflow-hidden p-0 transition-transform duration-300
+                    ease-in-out group"
+                    >
+                        <div className="relative flex items-center justify-center h-6 w-6 cursor-pointer">
+                            <Plug2 className="absolute h-5 w-5 text-white transition-all duration-300 ease-in-out group-hover:opacity-0 group-hover:rotate-90" />
+                            <Plus className="absolute h-5 w-5 text-white opacity-0 -rotate-90 transition-all duration-300 ease-in-out group-hover:opacity-100 group-hover:rotate-0" />
+                        </div>
+                    </button>
+
                 </div>
             </div>
         )
     }
 
     function createDistributionNodeCard(distId) {
-        const parentId = Object.entries(nodes).find(([_, node]) =>
+        const parentId = Object.entries(nodes).find(([, node]) =>
             node.children.includes(distId)
         )[0];
 
         return (
-            <div className='card' key={distId}>
-                <span className="card-title">GridLab-D - {distId}</span>
-                <span className="card-data"><strong>ID:</strong> {distId}</span>
-                <span className="card-data"><strong>Parent:</strong> {parentId}</span>
-                <div className="icon-container delete" onClick={() => removeDistributionNode(distId)}>
-                    <Trash2 fill="red" className="icon delete-icon" />
+            <div className='relative flex flex-col gap-1 h-25 w-full p-4 bg-black/5 rounded-lg' key={distId}>
+                <span className="font-bold">GridLab-D - {distId}</span>
+                <span className="text-sm"><strong>ID:</strong> {distId}</span>
+                <span className="text-sm"><strong>Parent:</strong> {parentId}</span>
+                <div className="absolute right-4 bottom-4 flex flex-row gap-2 justify-center items-center">
+                    <div className="flex items-center justify-center h-6 w-6 cursor-pointer" onClick={() => removeDistributionNode(distId)}>
+                        <Trash2 className="h-5 w-5 text-[#fb4b4b]/75 fill-[#fb4b4b]/75 hover:text-[#fb4b4b] hover:fill-[#fb4b4b]" />
+                    </div>
                 </div>
+
             </div>
         )
     }
@@ -127,30 +137,36 @@ export default function ComponentsPage() {
 
     return (
         <Page metadata={'Components'}>
-            <div className="simulation-name">
+            <div className="flex flex-row items-center gap-2 w-full">
                 <span>Simulation Name:</span>
                 <input
+                    className="w-[500px]"
                     type="text"
                     value={simName}
                     onChange={(e) => setSimName(e.target.value)}
                     placeholder="Enter a simulation name"
                 />
             </div>
-            <div className="components-container">
-                <div className="node-list">
-                    <div className="node-list-header">
+            <div className="flex flex-row gap-4 h-full w-full grow min-h-0">
+                <div className="flex flex-col gap-2 flex-1 w-full h-full overflow-hidden">
+                    <div className="flex justify-between items-center font-bold h-8">
                         <span>Transmission Nodes</span>
-                        <button onClick={() => createTransmissionNode()}>Add Node<Plus /></button>
+                        <button
+                            className="flex flex-row items-center gap-1 px-2 w-fit min-h-8 h-8 
+                            bg-transparent text-corvid-blue border-2 border-corvid-blue 
+                            rounded-sm font-bold cursor-pointer"
+                            onClick={() => createTransmissionNode()}
+                        >Add Node<Plus /></button>
                     </div>
-                    <div className="node-list-items">
+                    <div className="flex flex-col grow min-h-0 overflow-y-auto gap-2 p-4">
                         {Object.values(nodes).map(node => createTransmissionNodeCard(node))}
                     </div>
                 </div>
-                <div className="node-list">
-                    <div className="node-list-header">
+                <div className="flex flex-col gap-2 flex-1 w-full h-full overflow-hidden">
+                    <div className="flex justify-between items-center font-bold h-8">
                         <span>Distribution Nodes</span>
                     </div>
-                    <div className="node-list-items">
+                    <div className="flex flex-col grow min-h-0 overflow-y-auto gap-2 p-4">
                         {Object.values(nodes)
                             .flatMap(node => node.children)
                             .map(id => createDistributionNodeCard(id))}
@@ -158,7 +174,12 @@ export default function ComponentsPage() {
                 </div>
             </div>
             {Object.keys(nodes).length > 0 &&
-                <button className="primary align-right" onClick={() => sendConfiguration()}>
+                <button
+                    className="flex flex-row items-center gap-1 self-end px-2 w-fit min-h-8 h-8 
+                            bg-corvid-primary text-white border-2 border-corvid-primary 
+                            rounded-sm font-bold cursor-pointer"
+                    onClick={() => sendConfiguration()}
+                >
                     Build Configuration
                 </button>
             }
--- client/vite.config.js
+++ client/vite.config.js
@@ -2,13 +2,17 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import { dirname } from 'path';
 import { fileURLToPath } from 'url';
+import tailwindcss from '@tailwindcss/vite';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
 export default defineConfig({
   root: __dirname,
-  plugins: [react()],
+  plugins: [
+    react(),
+    tailwindcss()
+  ],
   server: {
     proxy: {
       '/api': {
